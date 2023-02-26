f473d20faf7463d52c1ac7f0be444d77d0579817
Johnman67112 <joaovitorcucomo@gmail.com>
2023-02-26 02:26:18 -0300

    basic status update

--- main.go
+++ main.go
@@ -1,1 +1,27 @@
 package main
+
+import (
+	"log"
+	"os"
+
+	"github.com/dghubble/go-twitter/twitter"
+	"github.com/dghubble/oauth1"
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	godotenv.Load(".env")
+
+	config := oauth1.NewConfig(os.Getenv("CONSUMER_KEY"), os.Getenv("CONSUMER_SECRET_KEY"))
+	token := oauth1.NewToken(os.Getenv("TOKEN_KEY"), os.Getenv("TOKEN_SECRET_KEY"))
+	httpClient := config.Client(oauth1.NoContext, token)
+
+	client := twitter.NewClient(httpClient)
+
+	tweet, _, err := client.Statuses.Update("Any message", nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	log.Print(tweet.Text)
+}
